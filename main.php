23b6359b29eafdd6b80e8a76e5df09b0689e9536
Anmol Koirala <33083327+anmolk13@users.noreply.github.com>
2017-10-31 21:50:24 +0545

    some common changes in effect

--- main.php
+++ main.php
@@ -1,6 +1,55 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="sidebar.css">
+	    <script src="bootstrap/bootstrap.min.js"></script>
+	    <script src="JQuery.js"></script>
+</head>
+<body>
+<?php include "header.html";  ?>
 
-echo"aaau aaau";
+
+<nav class="navigation">
+  <ul class="mainmenu">
+    <li><a href="">New Order</a></li>
+    <li><a href="">Room</a>
+     <ul class="submenu">
+      <li><a href="room.php">Register Room</a></li>
+        <li><a href="">Booked Room</a></li>
+        <li><a href="">Checked In</a></li>
+        <li><a href="">Checked Out</a></li>
+        <li><a href="">Room Details</a></li>
+      </ul>
+      </li>
+    <li><a href="">View orders</a>
+     
+    </li>
+    <li><a href="">Contact us </a></li>
+  </ul>
+</nav>
 
 
- ?>
+<nav class="n">
+  <ul class="mainmenu">
+    <li><a href="">New Order</a></li>
+    <li><a href="">Room</a>
+     <ul class="submenu">
+        <li><a href="">Booked Room</a></li>
+        <li><a href="">Check In</a></li>
+        <li><a href="">Check Out</a></li>
+        <li><a href="">Room Details</a></li>
+      </ul>
+      </li>
+    <li><a href="">View orders</a>
+     
+    </li>
+    <li><a href="">Contact us </a></li>
+  </ul>
+</nav>
+
+
+</body>
+<?php include "footer.html";?>
+</html>
