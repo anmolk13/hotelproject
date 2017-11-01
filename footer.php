b9ddf41845add9d062332f4fdeba991632b8a14d
Thee Nes K Cresta <dinesh111shrestha@gmail.com>
2017-11-01 19:16:32 +0545

    file extension changed to php

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	  <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+
+	  <style type="text/css">
+    .navbar-fixed-bottom {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+      text-align:center;
+        font-family: 'Raleway', sans-serif;
+    }
+    </style>
+</head>
+<body>
+
+</body>
+ <footer class="navbar navbar-default navbar-fixed-bottom" >
+  <p > Hotel  @ CopyRight</p>
+</footer>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+  
+</head>
+<body>
+<div>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <!-- Branding Image -->
+                    <span class="navbar-brand" href="">
+                       Hotel and Lodge
+                    </span>
+                </div>
+                 <ul class="nav navbar-nav">
+                      <li ><a href="#">Home</a></li>
+                      <li><a href="#"></a></li>
+                      <li><a href="#">Page 1</a></li>
+                    </ul>
+
+                     <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"> Sign Up</a></li>
+      <li><a href="#"></span> Login</a></li>
+    </ul>
+            </div>
+            </nav>
+</div>
+</body>
+</html>
